0153b8c8655f31c226f24070121862c0eadf282c
Andrei Beliaev <abbot190414@gmail.com>
2023-06-26 23:03:59 +0300

    homework 01: two shop items pages added

--- homework01/app.py
+++ homework01/app.py
@@ -6,7 +6,63 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    ...
+    context = {
+        'title': 'Welcome'
+    }
+    return render_template('index.html', **context)
+
+
+@app.route('/boots/')
+def boots_page():
+    context = {
+        'title': 'Boots',
+        'item_cards': [
+            {'image_path': '../static/img/boots.jpeg',
+             'card_title': 'Doom boots',
+             'description': 'Lorem ipsum dolor sit amet. 1'},
+            {'image_path': '../static/img/boots.jpeg',
+             'card_title': 'Doom boots',
+             'description': 'Lorem ipsum dolor sit amet. 2'},
+            {'image_path': '../static/img/boots.jpeg',
+             'card_title': 'Doom boots',
+             'description': 'Lorem ipsum dolor sit amet. 3'},
+            {'image_path': '../static/img/boots.jpeg',
+             'card_title': 'Doom boots',
+             'description': 'Lorem ipsum dolor sit amet. 4'},
+            {'image_path': '../static/img/boots.jpeg',
+             'card_title': 'Doom boots',
+             'description': 'Lorem ipsum dolor sit amet. 5'},
+        ]
+    }
+    return render_template('boots.html', **context)
+
+
+@app.route('/jackets/')
+def jackets_page():
+    context = {
+        'title': 'Jackets',
+        'item_cards': [
+            {'image_path': '../static/img/jacket.jpg',
+             'card_title': 'Doom jacket',
+             'description': 'Lorem ipsum dolor sit amet. 1'},
+            {'image_path': '../static/img/jacket.jpg',
+             'card_title': 'Doom jacket',
+             'description': 'Lorem ipsum dolor sit amet. 2'},
+            {'image_path': '../static/img/jacket.jpg',
+             'card_title': 'Doom jacket',
+             'description': 'Lorem ipsum dolor sit amet. 3'},
+            {'image_path': '../static/img/jacket.jpg',
+             'card_title': 'Doom jacket',
+             'description': 'Lorem ipsum dolor sit amet. 4'},
+            {'image_path': '../static/img/jacket.jpg',
+             'card_title': 'Doom jacket',
+             'description': 'Lorem ipsum dolor sit amet. 5'},
+            {'image_path': '../static/img/jacket.jpg',
+             'card_title': 'Doom boots',
+             'description': 'Lorem ipsum dolor sit amet. 6'},
+        ]
+    }
+    return render_template('jackets.html', **context)
 
 
 if __name__ == '__main__':
