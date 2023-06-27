4737d60cd3ff7bca195e191446b455e96f83d60f
Andrei Beliaev <abbot190414@gmail.com>
2023-06-27 08:56:11 +0300

    homework 01: pages clothes.html, contacts.html added

--- homework01/app.py
+++ homework01/app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index/')
 def main():
     context = {
         'title': 'Welcome'
@@ -63,6 +64,39 @@ def jackets_page():
         ]
     }
     return render_template('jackets.html', **context)
+
+
+@app.route('/clothes/')
+def clothes_page():
+    content = {
+        'title': 'Clothes catalog',
+        'item_cards': [
+            {'image_path': '../static/img/jacket.jpg',
+             'card_title': 'Jackets',
+             'link': '/jackets/',
+             },
+            {'image_path': '../static/img/boots.jpeg',
+             'card_title': 'Boots',
+             'link': '/boots/',
+             },
+        ]
+    }
+    return render_template('clothes.html', **content)
+
+
+@app.route('/contacts/')
+def contacts_page():
+    contacts_content = {
+        'title': 'Contacts',
+        'address': {
+            'country': 'Country',
+            'city': 'City',
+            'street': 'Street',
+            'office': 'room. 32',
+        },
+        'phone': '+7 (888) 999-77-55'
+    }
+    return render_template('contacts.html', contacts_content=contacts_content)
 
 
 if __name__ == '__main__':
